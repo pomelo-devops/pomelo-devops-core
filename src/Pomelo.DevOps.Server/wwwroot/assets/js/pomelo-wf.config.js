268d86e38c7c4a187ce5706a691baaff34b20a1b
Yuko Zheng <hi@yuko.me>
2023-01-23 18:33:00 +0800

    WIP: implement stage workflow node

--- src/Pomelo.DevOps.Server/wwwroot/assets/js/pomelo-wf.config.js
+++ src/Pomelo.DevOps.Server/wwwroot/assets/js/pomelo-wf.config.js
@@ -2,5 +2,6 @@
     { key: 'start', path: '/workflow/start/index' },
     { key: 'parallel', path: '/workflow/parallel/index' },
     { key: 'sync', path: '/workflow/sync/index' },
+    { key: 'stage', path: '/workflow/stage/index' },
     { key: 'finish', path: '/workflow/finish/index' }
 ];
