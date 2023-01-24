6cac1d615ea0845f2e49cd6c9863f4f0648d3857
Yuko Zheng <hi@yuko.me>
2023-01-24 17:47:55 +0800

    WIP: Create diagram pipeline stage def

---
+++ src/Pomelo.DevOps.Server/wwwroot/pages/pipeline/edit/stages-create.js
@@ -0,0 +1,38 @@
+﻿Page({
+    style: true,
+    data() {
+        return {
+            form: {
+                name: null,
+                description: null
+            }
+        };
+    },
+    created() {
+        this.form.name = this.$root.localization.sr('PIPELINE_EDIT_PLAYBOOK_ADD_STAGE_DEFAULT');
+    },
+    methods: {
+        async create() {
+
+            if (!this.form.name) {
+                notification.push('Create Stage Definition', 'Name cannot be empty', 'error');
+                return;
+            }
+
+            var notify = notification.push('Create Stage Definition', 'Creating...');
+            try {
+                var result = await Pomelo.CQ.Post(`/api/project/${this.projectId}/pipeline/${this.pipelineId}/diagram-stage`, this.form);
+                notify.type = 'success';
+                notify.message = 'Stage definition has been created';
+                notify.time = 10;
+
+                this.$parent.getStages();
+                this.$parent.editStage(result.data);
+            } catch (ex) {
+                notify.type = 'error';
+                notify.message = ex.message;
+                notify.time = 10;
+            }
+        }
+    }
+});
---
+++ src/Pomelo.DevOps.Server/wwwroot/pages/pipeline/edit/stages.js
@@ -0,0 +1,30 @@
+﻿Page({
+    layout: '/shared/devops',
+    style: true,
+    data() {
+        return {
+            stages: []
+        };
+    },
+    mounted() {
+        this.$root.ui.active = 'pipeline-stages';
+        this.$container('#pipeline-stages-main');
+    },
+    unmounted() {
+        this.$root.ui.active = null;
+    },
+    created() {
+        this.getStages();
+    },
+    methods: {
+        async getStages() {
+            this.stages = (await Pomelo.CQ.Get(`/api/project/${this.projectId}/pipeline/${this.pipelineId}/diagram-stage`)).data;
+        },
+        editStage(stage) {
+
+        },
+        createStage() {
+            this.$containers[0].open('/pages/pipeline/edit/stages-create');
+        }
+    }
+});
